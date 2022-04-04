885e9bf9da30092135f81d7854b7edd8843ef0a1
Hisham Taj <MrHishamTaj@gmail.com>
2022-04-04 17:55:20 +0100

    Fix linting errors

--- produceVideo.py
+++ produceVideo.py
@@ -1,10 +1,11 @@
-import cv2 
+import cv2
 
 frame = cv2.imread("images/frame_0.png")
 height, width, _ = frame.shape
 
 fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-compiler = cv2.VideoWriter("Wave_Propagation_Video.mp4", fourcc, 20, (width, height))
+compiler = cv2.VideoWriter("Wave_Propagation_Video.mp4", fourcc, 20,
+                           (width, height))
 
 
 for index in range(751):
