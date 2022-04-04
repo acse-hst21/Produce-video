a5bba7e38c2e5f3486f5a42ca9e087d646ff49c3
Hisham Taj <MrHishamTaj@gmail.com>
2022-04-04 17:51:20 +0100

    Fix linting errors

--- produceFrames.py
+++ produceFrames.py
@@ -1,11 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import os
 
 for index in range(751):
     filename = f"./data/output_{index}.dat"
 
-    output = np.loadtxt(filename, unpack = True)
+    output = np.loadtxt(filename, unpack=True)
 
     fig = plt.figure(figsize=(6, 3.2))
 
@@ -22,4 +21,3 @@ for index in range(751):
     plt.colorbar(orientation='vertical')
 
     fig.savefig(f"images/frame_{index}.png", dpi=fig.dpi)
-    
